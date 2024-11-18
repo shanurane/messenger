107ba02fa2222f99bc4f0e618057de33021f4b6f
shanurane <shanurane1020@gmail.com>
2024-11-18 08:43:19 +0530

    second

--- app/components/sidebar/MobileFooter.js
+++ app/components/sidebar/MobileFooter.js
@@ -3,28 +3,48 @@
 import useConversation from "@/app/hooks/useConversation";
 import useRoutes from "@/app/hooks/useRoutes";
 
-import MobileItem from "./MobileItem";
+import { useState } from "react";
 
-const MobileFooter = () => {
+import MobileItem from "./MobileItem";
+import Avatar from "@/app/components/Avatar";
+import SettingsModal from "./SettingsModal";
+
+const MobileFooter = ({ currentUser }) => {
   const routes = useRoutes();
   const { isOpen } = useConversation();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   if (isOpen) {
     return null;
   }
 
   return (
-    <div className="fixed justify-between w-full bottom-0 z-40 flex items-center bg-white border-t-[1px] lg:hidden">
-      {routes.map((route) => (
-        <MobileItem
-          key={route.href}
-          href={route.href}
-          active={route.active}
-          icon={route.icons}
-          onClick={route.onClick}
-        />
-      ))}
-    </div>
+    <>
+      <SettingsModal
+        currentUser={currentUser}
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      />
+      <div className="fixed flex justify-around w-full bottom-0 z-40 gap-x-2 items-center bg-white border-t-[1px] lg:hidden">
+        {routes.map((route) => (
+          <div key={route.href}>
+            <MobileItem
+              href={route.href}
+              active={route.active}
+              icon={route.icons}
+              onClick={route.onClick}
+            />
+          </div>
+        ))}
+
+        <div
+          onClick={() => setIsModalOpen(true)}
+          className="cursor-pointer hover:opacity-75 transition"
+        >
+          <Avatar user={currentUser} />
+        </div>
+      </div>
+    </>
   );
 };
 
--- app/components/sidebar/MobileItem.js
+++ app/components/sidebar/MobileItem.js
@@ -6,20 +6,20 @@ import clsx from "clsx";
 const MobileItem = ({ href, icon: Icon, active, onClick }) => {
   const handleClick = () => {
     if (onClick) {
-      return onClick();
+      return onClick;
     }
   };
 
   return (
     <Link
-      onClick={onClick}
+      onClick={handleClick}
       href={href}
       className={clsx(
-        `group flex gap-x-3 text-sm leading-6 font-semibold w-full justify-center p-4 text-gray-500 hover:text-black hover:bg-gray-100`,
+        `group flex gap text-sm leading-6 font-semibold w-full justify-center p-4 text-gray-600 hover:text-black hover:bg-gray-100`,
         active && "bg-gray-100 text-black"
       )}
     >
-      <Icon />
+      <Icon size={24} />
     </Link>
   );
 };
